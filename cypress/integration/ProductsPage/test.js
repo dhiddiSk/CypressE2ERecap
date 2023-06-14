f6e530d4c6c22349cb9c7e602d58f922df3ccb59
saikrishna <saikrishna.4369@gmail.com>
2023-06-14 16:43:25 +0200

    update

--- cypress/integration/ProductsPage/test.js
+++ cypress/integration/ProductsPage/test.js
@@ -1,23 +1,42 @@
 /// <reference types="Cypress" />
 import {
-    Given,
-    When,
-    Then,
-    And,
-    But
-  } from "cypress-cucumber-preprocessor/steps";
+  Given,
+  When,
+  Then,
+  And,
+  But
+} from "cypress-cucumber-preprocessor/steps";
 
-Given(/^I should be in the ecommerce application \"([^\"]*)\" page.$/, function (webPage) {
-     cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
-     cy.pageValidation("ecommerceHomePage");
-});
-  
-Then(/^I check if all the products are displayed as per requirements.$/, function () {
-     cy.fixture('data.json').then((testDataSetFixture) => {
-      console.log(testDataSetFixture.productsAndPrices);
-     })
-});
+Given(
+  /^I should be in the ecommerce application \"([^\"]*)\" page.$/,
+  function (webPage) {
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+    cy.pageValidation("ecommerceHomePage");
+  }
+);
 
-Then(/^I check if all the products are displayed correctly with correct price tags.$/, function () {
+Then(
+  /^I check if all the products are displayed as per requirements.$/,
+  function () {
+    cy.fixture("data.json").then((testDataSetFixture) => {
+
+        let testBasisDataObject = testDataSetFixture.productsAndPrices;
+        console.log(typeof(testBasisDataObject));
+      for(const item of Object.values(testBasisDataObject)){
+          console.log(item);
+      }
       
-});
+      // cy.get(".product .product-name").each(($el, index, $list) => {
+      //        cy.get($el).then($value => {
+      //         let itemsText = $value.text();
+      //         console.log(itemsText);
+      //        })
+      // });
+    });
+  }
+);
+
+Then(
+  /^I check if all the products are displayed correctly with correct price tags.$/,
+  function () {}
+);
